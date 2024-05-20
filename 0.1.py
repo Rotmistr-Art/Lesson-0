87eb9cc6f159701678c07fe6f7379250d17987d9
Rotmistr-Art <tattooer74@yandex.ru>
2024-05-20 13:18:15 +0500

    Add files via upload

---
+++ 0.1.py
@@ -0,0 +1,30 @@
+# длиина слова
+a = "привет мир"
+print(len(a))
+
+# суумы и разности
+first = 37
+second = 14
+print(first + second, first - second)
+summa = 51
+diff = 23
+print(summa + diff)
+
+# среднее арифметическое
+first = 1
+second = 2
+third = 3
+mean = (1 + 2 + 3)/3
+print(mean)
+
+# простые строчки
+first_string = 'Понедельник'
+second_string = 'Вторник'
+print(first_string,',' + second_string)
+
+# сложная формула
+a = 8
+b = 2
+c = 9
+f = (a*b) + (a*c)
+print(f ** 3 / 2)
